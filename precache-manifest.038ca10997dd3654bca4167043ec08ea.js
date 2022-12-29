28058f96efce13d92286e46213a6a80b1971e6b6
Code Sandbox <hello@codesandbox.io>
2022-12-29 22:21:29 +0000

    Deployment from Codesandbox.

---
+++ precache-manifest.038ca10997dd3654bca4167043ec08ea.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b9496843bacbf5c70b5f8af4b9d0bfcf",
+    "url": "https://prathapkammeta.github.io/csb-q4jnv7/index.html"
+  },
+  {
+    "revision": "ea9f9ac9968e7fcea27b",
+    "url": "https://prathapkammeta.github.io/csb-q4jnv7/static/css/main.3c703510.chunk.css"
+  },
+  {
+    "revision": "a24f21926391747ced4e",
+    "url": "https://prathapkammeta.github.io/csb-q4jnv7/static/js/2.439be825.chunk.js"
+  },
+  {
+    "revision": "d21b4bc0702397bf916425d3b382139b",
+    "url": "https://prathapkammeta.github.io/csb-q4jnv7/static/js/2.439be825.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ea9f9ac9968e7fcea27b",
+    "url": "https://prathapkammeta.github.io/csb-q4jnv7/static/js/main.5b69b368.chunk.js"
+  },
+  {
+    "revision": "f690c4180f45e6d8bd3a",
+    "url": "https://prathapkammeta.github.io/csb-q4jnv7/static/js/runtime-main.6d8cd854.js"
+  }
+]);
